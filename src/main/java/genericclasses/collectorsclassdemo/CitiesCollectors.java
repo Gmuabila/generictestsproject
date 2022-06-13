9e2027773f20c5e231bfa06e9a3fe72708d98b11
Jean-Rene Lingofe <rene.lingofe@proxytechnology.net>
2022-06-13 02:12:33 +0100

    Added tests

--- src/main/java/genericclasses/collectorsclassdemo/CitiesCollectors.java
+++ src/main/java/genericclasses/collectorsclassdemo/CitiesCollectors.java
@@ -1,2 +1,39 @@
-package genericclasses.collectorsclassdemo;public class CitiesCollectors {
+package genericclasses.collectorsclassdemo;
+
+
+/*Let’s consider a City class, which has attributes like name and temperature which are being initialized with the
+parameterized constructor. We will observe the different methods available in the collectors class using this example.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Java program to implement a
+// City class
+// Defining a city class
+public class CitiesCollectors {
+        // Initializing the properties
+        // of the city class
+        private String name;
+        private double temperature;
+        // Parameterized constructor to
+        // initialize the city class
+        public CitiesCollectors(String name, double temperature){
+            this.name = name;
+            this.temperature = temperature;
+        }
+        // Getters to get the name and
+        // temperature
+        public String getName(){
+            return name;
+        }
+        public double getTemperature(){
+            return temperature;
+        }
+        // Overriding the toString method
+        // to return the name and temperature
+        @Override
+        public String toString(){
+            return name + " --> " + temperature;
+        }
 }
--- src/test/java/genericclasses/methodreferencedemo/MethodReferenceTest.java
+++ src/test/java/genericclasses/methodreferencedemo/MethodReferenceTest.java
@@ -13,10 +13,13 @@ public class MethodReferenceTest {
     public void methodReferenceTesting(){
         List<String> messages = Arrays.asList("hello", "my", "dear", "friend");
 
-//       messages.forEach(word -> StringUtils.capitalize(word));
+//       messages.forEach(word -> StringUtils.capitalize(word));  //Does not convert Strings to Uppercase. To investigate.
 //       System.out.println(messages);
+
+//        messages.forEach(word -> word.toUpperCase());  //Does not convert Strings to UpperCase.  To investigate.
+//        System.out.println(messages);
+
         List<String> messagesToUpperCase = messages.stream().map(String::toUpperCase).collect(Collectors.toList());
         System.out.println(messagesToUpperCase);
-
     }
 }
