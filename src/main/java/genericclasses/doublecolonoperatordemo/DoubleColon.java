8ea3df15ae1c5e6a6188822ea1ccbc1fc2c5b611
Jean-Rene Lingofe <rene.lingofe@proxytechnology.net>
2022-06-16 08:53:00 +0100

    Added Double colon operator tests

---
+++ src/main/java/genericclasses/doublecolonoperatordemo/DoubleColon.java
@@ -0,0 +1,12 @@
+package genericclasses.doublecolonoperatordemo;
+
+public class DoubleColon {
+
+    static void someFunction(String s){
+        System.out.println(s);
+    }
+
+    public void instanceFunction(String s){
+        System.out.println(s);
+    }
+}
--- src/test/java/genericclasses/collectorsclassdemo/CollectorsClassTest.java
+++ src/test/java/genericclasses/collectorsclassdemo/CollectorsClassTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import genericclasses.collectorsclassdemo.CitiesCollectors;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CollectorsClassTest {
@@ -11,7 +13,7 @@ public class CollectorsClassTest {
     public static List<CitiesCollectors> prepareTemperature() {
 
         List<CitiesCollectors> citiesList = new ArrayList<>();
-        citiesList.add(new CitiesCollectors("New Delhi", 33.5));
+        citiesList.add(new CitiesCollectors("London", 33.5));
         citiesList.add(new CitiesCollectors("Kinshasa", 43));
         citiesList.add(new CitiesCollectors("New York", 13));
         citiesList.add(new CitiesCollectors("Dubai", 47));
@@ -38,7 +40,6 @@ public class CollectorsClassTest {
 //                .collect(Collectors.toList()));
         List<CitiesCollectors> citiesCollectorsList = prepareTemperature();
 
-
         List<String> citiesNames = citiesCollectorsList.stream().filter(cities -> cities.getTemperature() > 40).map(cities -> cities.getName()).collect(Collectors.toList());
         System.out.println("Names of Cities with Temperature greater than 40 are: ");
         System.out.println(citiesNames);
@@ -54,6 +55,35 @@ public class CollectorsClassTest {
         System.out.println("\nCities with Temperatures less than 5 Degrees are: ");
         System.out.println(prepareTemperature().stream().filter(city -> city.getTemperature() < 5).collect(Collectors.toList()));
         System.out.println();
+
+        //The following will return a Set instead of a list (Note the return type is Set of String Names)
+        System.out.println("Displaying names of cities with Temperature above 10");
+        System.out.println(prepareTemperature().stream().filter(cities -> cities.getTemperature() > 10).map(cities -> cities.getName()).collect(Collectors.toSet()));
+        System.out.println("Notice there's no duplicate in the Set");
+        System.out.println();
+
+        Set<CitiesCollectors> returnedSet = citiesCollectorsList.stream().filter(cities -> cities.getTemperature() > 1).collect(Collectors.toSet());
+        System.out.println("Cities with Temperature greater than 1 are: \n" + returnedSet);
+        System.out.println("Notice Duplicate here, shouldn't be");
+        System.out.println();
+
+        /*Returning Map with toMap() method.  Since our list contains duplicates, it will not filter out silently as toMap().
+        Instead, it throws an IllegalStateException. We can avoid and fix this issue by avoiding the key collision(in case of duplicate keys) with
+        the third argument that is BinaryOperator.  Binary operator specifies, how can we handle the collision,
+        and will pick either of the colliding elements.*/
+        Map<String, Double> returnedMap = citiesCollectorsList.stream().filter(cities -> cities.getTemperature() > 3)
+                .collect(Collectors.toMap(CitiesCollectors::getName, CitiesCollectors::getTemperature, (key, identicalKey) -> key));
+        System.out.println("Cities with Temp greater than 3 degrees in Map: \n" + returnedMap);
+        System.out.println();
+
+        System.out.println("Counting.....Collects the elements and counts the occurrences");
+        System.out.println(prepareTemperature().stream()
+                .collect(Collectors.groupingBy(CitiesCollectors::getName, Collectors.collectingAndThen(
+                                Collectors.counting(), f -> f.intValue()))));
+
+        //The grouping of elements is done as per the passed classifier function and returns the Collector with result in a Map.
+        System.out.println("\nUsing grouping....the result is a Map");
+        System.out.println(prepareTemperature().stream().collect(Collectors.groupingBy(CitiesCollectors::getName)));
     }
 }
 
